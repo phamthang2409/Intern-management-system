be21bfad2487082862e692cf780faea85e592066
phamthang2409 <149677255+phamthang2409@users.noreply.github.com>
2024-07-29 20:35:13 +0700

    task

--- Intern_manager_system/src/User/Task.java
+++ Intern_manager_system/src/User/Task.java
@@ -1,5 +1,82 @@
 package User;
+import java.util.Date;
 
+@Entity
+@Table(name = "TASKS")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "deadline")
+    private Date deadline;
+
+    @ManyToOne
+    @JoinColumn(name = "training_program_id")
+    private TrainingProgram trainingProgram;
+
+    @ManyToOne
+    @JoinColumn(name = "trainee_id")
+    private Trainee trainee;
+
+    public Task() {}
+
+    public Task(String name, String description, Date deadline, TrainingProgram trainingProgram, Trainee trainee) {
+        this.name = name;
+        this.description = description;
+        this.deadline = deadline;
+        this.trainingProgram = trainingProgram;
+        this.trainee = trainee;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public TrainingProgram getTrainingProgram() {
+        return trainingProgram;
+    }
+
+    public void setTrainingProgram(TrainingProgram trainingProgram) {
+        this.trainingProgram = trainingProgram;
+    }
+
+    public Trainee getTrainee() {
+        return trainee;
+    }
+
+    public void setTrainee(Trainee trainee) {
+        this.trainee = trainee;
+    }
 }
