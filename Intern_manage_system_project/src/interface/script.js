a3627d9916ac2a49ad899b579e4043e30e2cb050
ntq <tranquynhnguyen2004@gmail.com>
2024-08-02 04:24:34 +0700

    chỉnh sửa giao diện cơ bản

--- Intern_manage_system_project/src/interface/script.js
+++ Intern_manage_system_project/src/interface/script.js
@@ -1,37 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get the modals
+
     var registerModal = document.getElementById("registerModal");
     var loginModal = document.getElementById("loginModal");
 
-    // Get the buttons that open the modals
+
     var registerBtn = document.getElementById("registerBtn");
     var loginBtn = document.getElementById("loginBtn");
 
-    // Get the <span> elements that close the modals
+
     var closeRegister = document.getElementById("closeRegister");
     var closeLogin = document.getElementById("closeLogin");
 
-    // When the user clicks the button, open the register modal
+
     registerBtn.onclick = function() {
         registerModal.style.display = "block";
     }
 
-    // When the user clicks the button, open the login modal
+
     loginBtn.onclick = function() {
         loginModal.style.display = "block";
     }
 
-    // When the user clicks on <span> (x), close the register modal
+
     closeRegister.onclick = function() {
         registerModal.style.display = "none";
     }
 
-    // When the user clicks on <span> (x), close the login modal
+
     closeLogin.onclick = function() {
         loginModal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
+
     window.onclick = function(event) {
         if (event.target == registerModal) {
             registerModal.style.display = "none";
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    // Password confirmation for register form
+    // kiểm tra lại mật khẩu
     var form = document.querySelector("form");
     var password = document.getElementById("register_password");
     var confirmPassword = document.getElementById("confirm_password");
@@ -51,5 +51,16 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault();
             alert("Mật khẩu và xác nhận mật khẩu không khớp.");
         }
+    });
+});
+/*đăng xuất*/
+document.addEventListener('DOMContentLoaded', function() {
+    const logoutButton = document.getElementById('logout-button');
+
+    logoutButton.addEventListener('click', function() {
+        // Xử lý sự kiện đăng xuất
+        // Ví dụ: Xóa token xác thực, chuyển hướng đến trang đăng nhập, v.v.
+        alert('Bạn đã đăng xuất');
+        window.location.href = 'index.html'; // Chuyển hướng đến trang chủ
     });
 });
